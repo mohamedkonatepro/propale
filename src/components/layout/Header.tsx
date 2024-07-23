11e47be58a17ae694f558a8b4974eb46dae37356
mohamedkonate <mohamedkonate.proo@gmail.com>
2024-07-23 09:12:25 +0200

    custom header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,7 +4,9 @@ const Header = ({ title, subtitle, siren }: { title?: string, subtitle: string, 
   return (
     <div className='flex flex-col'>
       <div className="flex items-center mb-6">
-        <Image src="/entreprise.png" alt="Propale" width={68} height={68} className="mr-2" />
+        <div className="rounded-full bg-blue-950 w-24 h-24 flex items-center justify-center mr-2">
+          <Image src="/entreprise.png" alt="User" width={40} height={40} />
+        </div>
         <div className='flex flex-col'>
           <h2 className="text-3xl font-bold">{title}</h2>
           <p className='text-stone-400'>SIREN : {siren}</p>
