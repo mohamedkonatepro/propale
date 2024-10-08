cb69e47b62c8df36eb5c9511a1248dada8482730
mohamedkonate <mohamedkonate.proo@gmail.com>
2024-10-09 01:22:08 +0200

    share user fix

--- src/components/common/ProfileAvatarGroup.tsx
+++ src/components/common/ProfileAvatarGroup.tsx
@@ -20,9 +20,7 @@ const ProfileAvatarGroup: React.FC<ProfileAvatarGroupProps> = ({ profiles, maxDi
       <div className="flex items-center space-x-[-15px]">
         {displayedProfiles.map(profile => (
           <div key={profile.id} className="w-7 h-7 rounded-full overflow-hidden border border-blue-200 shadow-custom-left">
-            {profile.blocked ? (
-              <Image src="/avatar.svg" alt={`${profile.firstname} ${profile.lastname}`} layout="fill" objectFit="cover" />
-            ) : (
+            {(
               <div className="shadow-2xl w-full h-full flex items-center justify-center bg-blue-200 text-blueCustom text-xs shadow-custom-left">
                 {getInitials(profile.firstname, profile.lastname)}
               </div>
