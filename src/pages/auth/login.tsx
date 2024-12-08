d60e2813c5626cc1e7807f559a71efbc2853505d
mohamedkonate <mohamedkonate.proo@gmail.com>
2024-12-08 21:15:34 +0100

    show password

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,8 +1,8 @@
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { LoginSchema } from '@/schemas/auth';
 import { supabase } from '@/lib/supabaseClient';
-import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import SocialLinks from '@/components/auth/SocialLinks';
 import CustomAlert from '@/components/common/Alert';
@@ -10,6 +10,7 @@ import LoginHeader from '@/components/auth/LoginHeader';
 import { FcGoogle } from "react-icons/fc";
 import { Button } from '@/components/common/Button';
 import handleSessionAndRedirect from '@/services/authService';
+import { FiEye, FiEyeOff} from "react-icons/fi";
 
 type LoginFormInputs = {
   email: string;
@@ -18,6 +19,7 @@ type LoginFormInputs = {
 
 const Login = () => {
   const [message, setMessage] = useState(['']);
+  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -81,12 +83,21 @@ const Login = () => {
                 <label className="block text-gray-700 text-sm font-bold mb-2">Mot de passe</label>
                 <a href="#" className="text-sm text-blueCustom" onClick={navigateToForgotPassword}>Mot de passe oublié ?</a>
               </div>
-              <input
-                type="password"
-                placeholder="********"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                {...register('password')}
-              />
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="********"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  {...register('password')}
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500"
+                >
+                  {showPassword ? <FiEyeOff /> : <FiEye />}
+                </button>
+              </div>
               {errors.password && <p className="text-red-500 text-xs italic">{errors.password.message}</p>}
             </div>
             <div className="flex items-center justify-center">
--- src/pages/auth/reset-password.tsx
+++ src/pages/auth/reset-password.tsx
@@ -1,13 +1,14 @@
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { ResetPasswordSchema } from '@/schemas/auth';
 import { supabase } from '@/lib/supabaseClient';
-import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import LoginHeader from '@/components/auth/LoginHeader';
 import SuccessAlert from '@/components/common/SuccessAlert';
 import SocialLinks from '@/components/auth/SocialLinks';
 import ValidationMessage from '@/components/common/ValidationMessage';
+import { FiEye, FiEyeOff} from "react-icons/fi";
 
 type ResetPasswordFormInputs = {
   password: string;
@@ -16,6 +17,8 @@ type ResetPasswordFormInputs = {
 
 const ResetPassword = () => {
   const [passwordReset, setPasswordReset] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const router = useRouter();
 
   const { register, handleSubmit, formState: { errors } } = useForm<ResetPasswordFormInputs>({
@@ -50,36 +53,54 @@ const ResetPassword = () => {
           </h3>
           {!passwordReset ? (
             <form className="w-full" onSubmit={handleSubmit(handleResetPassword)}>
-              <div className="mb-4">
+              <div className="mb-4 relative">
                 <label className="block text-gray-700 text-sm font-bold mb-2">
                   Choisir un nouveau mot de passe
                 </label>
-                <input
-                  type="password"
-                  placeholder="motdepasse"
-                  className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-                    errors.password ? 'border-red-500' : ''
-                  }`}
-                  {...register('password')}
-                />
-                {errors.password ? (
+                <div className="relative">
+                  <input
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Mot de passe"
+                    className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                      errors.password ? 'border-red-500' : ''
+                    }`}
+                    {...register('password')}
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500"
+                  >
+                    {showPassword ? <FiEyeOff /> : <FiEye />}
+                  </button>
+                </div>
+                {errors.password && (
                   <div className='mt-2'>
                     <ValidationMessage message='Votre mot de passe doit contenir au moins :' requirements={[
                       '8 caractères', 'Une majuscule', 'Un chiffre', 'Un caractère spécial'
                     ]} />
                   </div>
-                ) : ''}
+                )}
               </div>
-              <div className="mb-4">
+              <div className="mb-4 relative">
                 <label className="block text-gray-700 text-sm font-bold mb-2">
                   Confirmer le mot de passe
                 </label>
-                <input
-                  type="password"
-                  placeholder="Confirmer le mot de passe"
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  {...register('confirmPassword')}
-                />
+                <div className="relative">
+                  <input
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    placeholder="Confirmer le mot de passe"
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    {...register('confirmPassword')}
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                    className="absolute inset-y-0 right-0 pr-3 flex items-center text-gray-500"
+                  >
+                    {showConfirmPassword ? <FiEyeOff /> : <FiEye />}
+                  </button>
+                </div>
                 {errors.confirmPassword && <p className="text-red-500 text-xs italic">{errors.confirmPassword.message}</p>}
               </div>
               <div className="flex items-center justify-center">
