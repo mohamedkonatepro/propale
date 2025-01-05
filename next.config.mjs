728bfe85aedadb63c2ab4ee112852f153a3bcb6a
mohamedkonate <mohamedkonate.proo@gmail.com>
2025-01-05 12:15:48 +0100

    config nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/api/:path*',
+        has: [
+          {
+            type: 'header',
+            key: 'Host',
+            value: 'propale.co',
+          },
+        ],
+        destination: 'https://www.propale.co/api/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
